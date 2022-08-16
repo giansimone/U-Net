588d0a7f371e449ae0643df6eecdecb226d2af6e
Giansimone <giansimone@gmail.com>
2022-08-16 17:31:13 +0100

    Change normalisation in model.

--- senso/model.py
+++ senso/model.py
@@ -6,12 +6,19 @@ class ConvConv(nn.Module):
     """Double convolutional neuronal network for a U-Net."""
 
     def __init__(self, in_ch, out_ch) -> None:
+        """
+        Args:
+            in_ch: Number of input channels.
+            out_ch: Number of output channels.
+        """
         super(ConvConv, self).__init__()
         self.conv_op = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
+            nn.BatchNorm2d(out_ch),
             nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1, stride=1),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.BatchNorm2d(out_ch)
         )
     
     def forward(self, x):
@@ -23,66 +30,45 @@ class ConvConv(nn.Module):
 class UNet(nn.Module):
     """Convolutional neuronal network U-Net."""
 
-    def __init__(self, im_height, im_weight, in_ch=1, out_ch=3, dropout_rate=0.1) -> None:
+    def __init__(self, im_height=512, im_weight=512, in_ch=1, out_ch=3) -> None:
         """
         Args:
            im_height: Image height in pixels.
            im_weight: Image with in pixels.
            in_ch: Number of input channels.
            out_ch: Number of output channels (i.e., classes).
-           dropout_rate: Dropout normalisation. 
         Returns:
-            
+            None
         """
         super(UNet, self).__init__()
-        self.encode_A = ConvConv(1, 64)
+        self.encode_A = ConvConv(in_ch, 64)
         self.encode_B = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(dropout_rate),
             ConvConv(64, 128)
         )
         self.encode_C = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(dropout_rate),
             ConvConv(128, 256)
         )
         self.encode_D = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(dropout_rate),
             ConvConv(256, 512)
         )
         self.encode_E = nn.Sequential(
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Dropout(dropout_rate),
             ConvConv(512, 1024)
         )
-        self.upsample_A = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-            nn.Dropout(dropout_rate),
-            nn.Conv2d(1024, 512, kernel_size=2, padding=1, stride=1)
-        )
+        self.upsample_A = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
         self.decode_A = ConvConv(1024, 512)
-        self.upsample_B = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-            nn.Dropout(dropout_rate),
-            nn.Conv2d(512, 256, kernel_size=2, padding=1, stride=1)
-        )
+        self.upsample_B = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
         self.decode_B = ConvConv(512, 256)
-        self.upsample_C = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-            nn.Dropout(dropout_rate),
-            nn.Conv2d(256, 128, kernel_size=2, padding=1, stride=1)
-        )
+        self.upsample_C = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
         self.decode_C = ConvConv(256, 128)
-        self.upsample_D = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-            nn.Dropout(dropout_rate),
-            nn.Conv2d(128, 64, kernel_size=2, padding=1, stride=1)
-        )
+        self.upsample_D = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
         self.decode_D = ConvConv(128, 64)
         self.conv_out = nn.Sequential(
             nn.Conv2d(64, out_ch, kernel_size=1),
-            nn.Softmax()
+            nn.Softmax(dim=1)
         )
     
     def forward(self, x):
@@ -92,7 +78,7 @@ class UNet(nn.Module):
         down_D = self.encode_D(down_C)
         down_E = self.encode_E(down_D)
         x = self.upsample_A(down_E)
-        x = torch.cat([down_D, x], dim=1) #TO DO: Check if dim is correct.
+        x = torch.cat([down_D, x], dim=1)
         x = self.decode_A(x)
         x = self.upsample_B(x)
         x = torch.cat([down_C, x], dim=1)
